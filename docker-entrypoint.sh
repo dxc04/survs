9e265ce5950c6977a80f084c0cd069ac60789b44
Giovanni Petrache <giovanni.petrache@gmail.com>
2017-04-27 16:28:48 +0800

    Fix Docker Setup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-chown -R www-data:www-data /var/www/service/storage
-chmod -R 770 /var/www/service/storage
+chown -R www-data:www-data /var/www/service/storage /var/www/service/bootstrap/cache
+chmod -R 770 /var/www/service/storage /var/www/service/bootstrap/cache
 
 if [ "${1:0:1}" = '-' ]; then
 	set -- php-fpm "$@"
